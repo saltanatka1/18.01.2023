063a2eb0a31642cebe18a543ad47db765fa61ac9
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-01-18 15:10:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+let myName = "Saltanat"; 
+
+console.log(myName);
+console.log("My name's length is " + myName.length);
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());
+
+let pi = 3.141593;
+
+console.log(pi);
+console.log(pi.toFixed(2));
+console.log(pi.toFixed(0));
+
+let example1 = 2 + 2;
+let example2 = 2 - 2;
+let example3 = 2 * 2;
+let example4 = 2 / 2;
+let example5 = 2 ** 4; //степень
+let example6 = 5 % 2; //остаток от деления
+let example7 = 81 ** (1/2);//корень
+let example8 = "Hello " + 'World ' + `!`;
+
+let example9 = 4 + 10 + 12;
+let example10 = 1 + 5 * 3;
+let example11 = 15 * (1 - 1);
+
+let example12 = 5 + '3';
+let example13 = '5' + 3;
+let example14 = '5' * 3;//15
+let example15 = 'a' * 3;//NaN - не число
+
+let example16 = -15;
+let example17 = +'15';
+
+console.log(example1);
+console.log(example2);
+console.log(example3);
+console.log(example4);
+console.log(example5);
+console.log(example6);
+console.log(example7);
+console.log(example8);
+console.log(example9);
+console.log(example10);
+console.log(example11);
+console.log(example12);
+console.log(example13);
+console.log(example14);
+console.log(example15);
+console.log(example16);
+console.log(example17);
